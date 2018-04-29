40b26924bc4bff21f118f789630f08bb40a417df
AleDema <38495515+AleDema@users.noreply.github.com>
2018-04-29 04:14:12 +0200

    Update missionGameplay.c

--- scripts/5_Mission/mission/missionGameplay.c
+++ scripts/5_Mission/mission/missionGameplay.c
@@ -1502,10 +1502,7 @@ class MissionGameplay extends MissionBase
 		{
 			
 			m_killed_infected = DayZInfected.getDeath();
-					if(m_killed_infected == backup){
-						m_oPlayer.MessageStatus( "no new zombies killed" );
-					}
-					else {
+					if(m_killed_infected != backup)
 						//m_oPlayer.MessageStatus( m_killed_infected.ToString() ); DEBUG
 						backup = m_killed_infected;
 						m_spawned_infected = m_spawned_infected - m_killed_infected;
