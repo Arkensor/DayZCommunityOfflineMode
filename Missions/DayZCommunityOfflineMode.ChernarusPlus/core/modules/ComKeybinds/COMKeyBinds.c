7dcbdb5b1b89317b6bf1e228c60facf6af20fc1d
Arkensor <arkensor@gmail.com>
2018-11-30 16:50:02 +0100

    Reimplemented autowalk and other 1.9 features Added shovel to default spawn gear

--- Missions/DayZCommunityOfflineMode.ChernarusPlus/core/modules/ComKeybinds/COMKeyBinds.c
+++ Missions/DayZCommunityOfflineMode.ChernarusPlus/core/modules/ComKeybinds/COMKeyBinds.c
@@ -4,7 +4,8 @@
 class COMKeyBinds extends Module 
 {
 	bool m_IsHudVisible = true;
-	
+	protected int m_nAutoWalkMode = 0;
+
 	void COMKeyBinds()
 	{
 	}
@@ -20,68 +21,39 @@ class COMKeyBinds extends Module
 	
 	override void onUpdate( float timeslice )
 	{
-        // just putting this here for now
-        if ( m_GodMode ) // located in staticfunctions
-        {
-            GetPlayer().SetAllowDamage( false );
+	    UpdateAutoWalk();
 
-            GetPlayer().SetHealth( GetPlayer().GetMaxHealth( "", "" ) );
-            GetPlayer().SetHealth( "","Blood", GetPlayer().GetMaxHealth( "", "Blood" ) );
-            GetPlayer().SetHealth( "","Shock", GetPlayer().GetMaxHealth( "", "Shock" ) );
-            
-            GetPlayer().GetStaminaHandler().SyncStamina(1000, 1000);
-            GetPlayer().GetStatStamina().Set(GetPlayer().GetStaminaHandler().GetStaminaCap());
-            GetPlayer().GetStatEnergy().Set(1000);
-            GetPlayer().GetStatWater().Set(1000);
-//            GetPlayer().GetStatStomachVolume().Set(300);
-//            GetPlayer().GetStatStomachWater().Set(300);
-//            GetPlayer().GetStatStomachEnergy().Set(300);
-            GetPlayer().GetStatHeatComfort().Set(0);
-            
-            
-            EntityAI oWeapon = GetPlayer().GetHumanInventory().GetEntityInHands();
-
-            if( oWeapon )
-            {
-                Magazine oMag = ( Magazine ) oWeapon.GetAttachmentByConfigTypeName( "DefaultMagazine" );
-
-                if( oMag && oMag.IsMagazine() )
-                {
-                    oMag.LocalSetAmmoMax();
-                }
-                
-                Object oSupressor = ( Object ) oWeapon.GetAttachmentByConfigTypeName( "SuppressorBase" );
-
-                if( oSupressor )
-                {
-                    oSupressor.SetHealth( oSupressor.GetMaxHealth( "", "" ) );
-                }
-            }
-        }
+	    UpdateGodMode();
 	}
 	
 	override void RegisterKeyMouseBindings() 
 	{
-		KeyMouseBinding toggleCursor    = new KeyMouseBinding( GetModuleType() , "ToggleCursor"  , "[U]"    , "Toggles the cursor."   , true );
-		KeyMouseBinding toggleCOMEditor = new KeyMouseBinding( GetModuleType() , "ShowCOMEditor" , "[Y]"    , "Opens the COM Editor."        );
-		KeyMouseBinding teleport	    = new KeyMouseBinding( GetModuleType() , "TeleportCursor", "[H]"    , "Teleport to cursor position." );
-		KeyMouseBinding reload          = new KeyMouseBinding( GetModuleType() , "Reload"        , "[R]"    , "Instantly reloads mag."		 );
-        KeyMouseBinding spawnZ          = new KeyMouseBinding( GetModuleType() , "SpawnZ"        , "[O]"    , "Spawns infected." );
-        KeyMouseBinding hideHud          = new KeyMouseBinding( GetModuleType() , "HideHud"        , "[HOME]"    , "Hides ui completely." );
+		KeyMouseBinding toggleCursor    = new KeyMouseBinding( GetModuleType() , "ToggleCursor"  , "[U]"    , "Toggles the cursor."   , true   );
+		KeyMouseBinding toggleCOMEditor = new KeyMouseBinding( GetModuleType() , "ShowCOMEditor" , "[Y]"    , "Opens the COM Editor."          );
+		KeyMouseBinding teleport	    = new KeyMouseBinding( GetModuleType() , "TeleportCursor", "[END]"    , "Teleport to cursor position."   );
+		KeyMouseBinding reload          = new KeyMouseBinding( GetModuleType() , "Reload"        , "[R]"    , "Instantly reloads mag."		   );
+        KeyMouseBinding spawnZ          = new KeyMouseBinding( GetModuleType() , "SpawnZ"        , "[O]"    , "Spawns infected."               );
+        KeyMouseBinding hideHud         = new KeyMouseBinding( GetModuleType() , "HideHud"       , "[HOME]" , "Hides ui completely."           );
+        KeyMouseBinding printPlayer     = new KeyMouseBinding( GetModuleType() , "PrintPlayer"   , "[P]"    , "Print current player position." );
+        KeyMouseBinding autoRun         = new KeyMouseBinding( GetModuleType() , "AutoRun"   , "[X]"    , "Toggle autorun." );
 
 		toggleCursor   .AddKeyBind( KeyCode.KC_U,    KeyMouseBinding.KB_EVENT_PRESS   );
 		toggleCOMEditor.AddKeyBind( KeyCode.KC_Y,    KeyMouseBinding.KB_EVENT_RELEASE );
-		teleport       .AddKeyBind( KeyCode.KC_H,    KeyMouseBinding.KB_EVENT_PRESS   );
+		teleport       .AddKeyBind( KeyCode.KC_END,  KeyMouseBinding.KB_EVENT_PRESS   );
 		reload         .AddKeyBind( KeyCode.KC_R,    KeyMouseBinding.KB_EVENT_RELEASE );
         spawnZ         .AddKeyBind( KeyCode.KC_O,    KeyMouseBinding.KB_EVENT_RELEASE );
         hideHud        .AddKeyBind( KeyCode.KC_HOME, KeyMouseBinding.KB_EVENT_RELEASE );
-		
+        printPlayer    .AddKeyBind( KeyCode.KC_P,    KeyMouseBinding.KB_EVENT_RELEASE );
+        autoRun        .AddKeyBind( KeyCode.KC_X,    KeyMouseBinding.KB_EVENT_RELEASE );
+
 		RegisterKeyMouseBinding( toggleCursor );
 		RegisterKeyMouseBinding( toggleCOMEditor );
 		RegisterKeyMouseBinding( teleport );
 		RegisterKeyMouseBinding( reload );
         RegisterKeyMouseBinding( spawnZ );
         RegisterKeyMouseBinding( hideHud );
+        RegisterKeyMouseBinding( printPlayer );
+        RegisterKeyMouseBinding( autoRun );
 	}
 
     void ToggleCursor()
@@ -161,12 +133,110 @@ class COMKeyBinds extends Module
 
     void SpawnZ() 
     {
-        GetGame().CreateObject( WorkingZombieClasses().GetRandomElement(), GetCursorPos(), false, true );
+        if( CTRL() )
+        {
+            GetGame().CreateObject( "Animal_CanisLupus_Grey", GetCursorPos(), false, true );
+        }
+        else if( SHIFT() )
+        {
+            GetGame().CreateObject( GetRandomChildFromBaseClass( "cfgVehicles", "AnimalBase" ), GetCursorPos(), false, true );
+        }
+        else
+        {
+            GetGame().CreateObject( WorkingZombieClasses().GetRandomElement(), GetCursorPos(), false, true );
+        }
     }
 
     void HideHud() 
     {
         Widget hudWidget = IngameHud.Cast(GetClientMission().GetHud()).GetHudPanelWidget();
         hudWidget.Show(!hudWidget.IsVisible());
+    }
+
+    void PrintPlayer()
+    {
+        Print( "Position:" + GetPlayer().GetPosition().ToString() );
+        Print( "Orientation" + GetPlayer().GetOrientation().ToString() );
+        Message( "POS X:" + GetPlayer().GetPosition()[0] + " Y:" + GetPlayer().GetPosition()[2] + " Z:" + GetPlayer().GetPosition()[1] );
+        Message( "Player position and orientation vector were written to the game logs too." );
+    }
+
+    void AutoRun()
+    {
+        if( m_nAutoWalkMode && !SHIFT() )
+        {
+            m_nAutoWalkMode = 0;
+            GetPlayer().GetInputController().OverrideMovementSpeed( false, 0 );
+            GetPlayer().GetInputController().OverrideMovementAngle( false, 0 );
+        }
+        else
+        {
+            if( SHIFT() )
+            {
+                m_nAutoWalkMode = 2;
+            }
+            else
+            {
+                m_nAutoWalkMode = 1;
+            }
+        }
+    }
+
+    void UpdateAutoWalk()
+    {
+        if( m_nAutoWalkMode )
+        {
+            if( ( GetPlayer().GetInputController().LimitsIsSprintDisabled() ) || ( m_nAutoWalkMode == 1 ) )
+            {
+                GetPlayer().GetInputController().OverrideMovementSpeed( true, 2 );
+            }
+            else
+            {
+                GetPlayer().GetInputController().OverrideMovementSpeed( true, 3 );
+            }
+
+            GetPlayer().GetInputController().OverrideMovementAngle( true, 1 );
+        }
+    }
+
+    void UpdateGodMode()
+    {
+        // just putting this here for now
+        if ( m_GodMode ) // located in staticfunctions
+        {
+            GetPlayer().SetAllowDamage( false );
+
+            GetPlayer().SetHealth( GetPlayer().GetMaxHealth( "", "" ) );
+            GetPlayer().SetHealth( "","Blood", GetPlayer().GetMaxHealth( "", "Blood" ) );
+            GetPlayer().SetHealth( "","Shock", GetPlayer().GetMaxHealth( "", "Shock" ) );
+
+            GetPlayer().GetStaminaHandler().SyncStamina(1000, 1000);
+            GetPlayer().GetStatStamina().Set(GetPlayer().GetStaminaHandler().GetStaminaCap());
+            GetPlayer().GetStatEnergy().Set(1000);
+            GetPlayer().GetStatWater().Set(1000);
+//            GetPlayer().GetStatStomachVolume().Set(300);
+//            GetPlayer().GetStatStomachWater().Set(300);
+//            GetPlayer().GetStatStomachEnergy().Set(300);
+            GetPlayer().GetStatHeatComfort().Set(0);
+
+            EntityAI oWeapon = GetPlayer().GetHumanInventory().GetEntityInHands();
+
+            if( oWeapon )
+            {
+                Magazine oMag = ( Magazine ) oWeapon.GetAttachmentByConfigTypeName( "DefaultMagazine" );
+
+                if( oMag && oMag.IsMagazine() )
+                {
+                    oMag.LocalSetAmmoMax();
+                }
+
+                Object oSupressor = ( Object ) oWeapon.GetAttachmentByConfigTypeName( "SuppressorBase" );
+
+                if( oSupressor )
+                {
+                    oSupressor.SetHealth( oSupressor.GetMaxHealth( "", "" ) );
+                }
+            }
+        }
     }
 }
