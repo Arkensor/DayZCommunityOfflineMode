464ee4b04580f31d34d15abf6b93c997cd9cd585
Arkensor <arkensor@gmail.com>
2018-12-13 20:26:21 +0100

    Adjusted params to enable logs

--- Missions/DayZCommunityOfflineMode.ChernarusPlus/DayZCommunityOfflineMode.bat
+++ Missions/DayZCommunityOfflineMode.ChernarusPlus/DayZCommunityOfflineMode.bat
@@ -6,4 +6,4 @@ RD /s /q "storage_-1" > nul 2>&1
 
 cd ../../
 
-start DayZ_x64.exe -mission=.\Missions\DayZCommunityOfflineMode.ChernarusPlus -scrAllowFileWrite -nosplash -noPause -noBenchmark -scriptDebug=true -filePatching
+start DayZ_x64.exe -mission=.\Missions\DayZCommunityOfflineMode.ChernarusPlus -scrAllowFileWrite -nosplash -noPause -noBenchmark -scriptDebug=true -filePatching -doLogs
