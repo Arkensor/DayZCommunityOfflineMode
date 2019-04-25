5ab3c69e432bc2ac396d64de6fa84e5d1c6536ab
Arkensor <arkensor@gmail.com>
2019-04-25 16:16:50 +0200

    Changed key for autorun back to x

--- Missions/DayZCommunityOfflineMode.ChernarusPlus/core/modules/ComKeybinds/COMKeyBinds.c
+++ Missions/DayZCommunityOfflineMode.ChernarusPlus/core/modules/ComKeybinds/COMKeyBinds.c
@@ -46,7 +46,7 @@ class COMKeyBinds extends Module
         spawnZ         .AddBinding( "kO" );
         hideHud        .AddBinding( "kHome" );
         printPlayer    .AddBinding( "kP" );
-        autoRun        .AddBinding( "kComma" );
+        autoRun        .AddBinding( "kX" );
         keyFrame       .AddBinding( "kPrior" );
         closeMenu      .AddBinding( "kEscape" );
 
